0cacddfbd7de836aeeb0ad75dc1354ed24683ea0
Berestenko Daniil <98707505+BerestenkoDaniil@users.noreply.github.com>
2023-06-15 12:35:21 +0300

    Implemented simple app that uses the humidity sensor

---
+++ Humidity_sensor/app/src/main/java/com/example/humidity_sensor/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.humidity_sensor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    private TextView textView;
+    private SensorManager sensorManager;
+    private Sensor humiditySensor;
+
+    private Boolean isHumiditySensor;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_RELATIVE_HUMIDITY) != null){
+            humiditySensor = sensorManager.getDefaultSensor(Sensor.TYPE_RELATIVE_HUMIDITY);
+            isHumiditySensor =true;
+        }else {
+            textView.setText("Humidity Sensor is not availible!");
+            isHumiditySensor = false;
+        }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        textView.setText(sensorEvent.values[0]+ "%");
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(isHumiditySensor){
+            sensorManager.registerListener(this, humiditySensor, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(isHumiditySensor){
+            sensorManager.unregisterListener(this);
+        }
+    }
+}
